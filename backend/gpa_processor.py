d0cd9e3905f6f969ffe5d666628e3ceccf2412e2
Cwooper <gibbonj9@wwu.edu>
2024-04-29 16:59:49 -0700

    Should be done with GPA processing

--- backend/gpa_processor.py
+++ backend/gpa_processor.py
@@ -7,14 +7,79 @@ import pandas as pd
 from Course import Course
 from data_refresh import fetch_terms_list, filter_terms
 
-code_pattern = re.compile(r'(.*?)\s\d+')
+code_pattern = re.compile(r'(.*?)\s(\d+[A-Z]?)$')
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 data_directory = os.path.join(current_directory, 'data')
 gpa_excel_file = os.path.join(data_directory, "Grade_Distribution_5_Year.xlsx")
 gpa_pickle_file = os.path.join(data_directory, "grades.pkl")
 
-columns_to_drop = ['TERM', 'CRN', 'TITLE', 'Students enrolled', 'Grade count']
+columns_to_drop = ['TERM', 'CRN', 'TITLE', 'Students enrolled', 'Grade count', 'CNT_W'] 
+grade_columns = ['CNT_A', 'CNT_AM', 'CNT_BP', 'CNT_B', 'CNT_BM', 'CNT_CP',
+                 'CNT_C', 'CNT_CM', 'CNT_DP', 'CNT_D', 'CNT_DM', 'CNT_F']
+gpa_values = np.array([4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0.0])
+
+mapping = {
+    'AHE':'CFPA',
+    'ASLC':'SPED',
+    'ARAB':'LANG',
+    'ASTR':'PHYS',
+    'BNS':'PSY',
+    'BUS':'ACCT',
+    'CHIN':'LANG',
+    'CLST':'LANG',
+    'CD':'HHD',
+    'CSEC':'CSE',
+    'C2C':'HCS',
+    'CISS':'CSCI',
+    'DNC':'THTR',
+    'DATA':'CSCI',
+    'DIAD':'EDUC',
+    'ECE':'ELED',
+    'EDAD':'SPED',
+    'ESJ':'SEC',
+    'EECE':'ENGD',
+    'ENGR':'ENGD',
+    'EUS':'LANG',
+    'FIN':'FMKT',
+    'FREN':'LANG',
+    'GERM':'LANG',
+    'GLBL':'ESCI',
+    'GREK':'LANG',
+    'HLED':'HHD',
+    'HRM':'MGMT',
+    'HSP':'HCS',
+    'HUMA':'GHR',
+    'ID':'ENGD',
+    'I T':'ECEM',
+    'IEP':'LANG',
+    'IBUS':'MGMT',
+    'ITAL':'LANG',
+    'JAPN':'LANG',
+    'KIN':'HHD',
+    'LAT':'LANG',
+    'MIS':'DSCI',
+    'MFGE':'ENGD',
+    'MKTG':'FMKT',
+    'MPAC':'ACCT',
+    'M/CS':'MATH',
+    'MLE':'ECEM',
+    'NURS':'HCS',
+    'OPS':'MBA',
+    'PA':'HHD',
+    'PE':'HHD',
+    'PEH':'HHD',
+    'PME':'ENGD',
+    'PORT':'LANG',
+    'RECR':'HHD',
+    'RC':'HCS',
+    'REL':'LBRL',
+    'RUSS':'LANG',
+    'SPAN':'LANG',
+    'SUST':'UEPP',
+    'TEOP':'ELIT',
+    'TESL':'ELED'
+}
 
 def excel_to_pickle():
     if not os.path.exists(gpa_excel_file):
@@ -41,17 +106,24 @@ def excel_to_pickle():
     df.to_pickle(gpa_pickle_file)
     print(f"Created {gpa_pickle_file}.")
 
-def calculate_gpa(row, gpa_df) -> int:
-    code_match = re.match(code_pattern, row['subject'])
-    code = None
-    if code_match:
-        code = code_match.group(1)
+def get_subject(code: str) -> str:
+    subject_match = re.match(code_pattern, code)
+    if subject_match:
+        subject = subject_match.group(1)
+        code = subject_match.group(2)
 
-    instructor = row['instructor']
+    # Replace any terms subjects with the associated in the gpa_df
+    for key in mapping.keys():
+        if key == subject:
+            subject = subject.replace(key, mapping[key])
+            break
+    
+    return subject + " " + code
 
+# Returns instructor first_name, last_name if it is found
+def get_instructor(instructor: str):
     first_name = None
     last_name = None
-
     if ', ' in instructor:
         names = instructor.split(',')
         last_names_str = names[0].strip()
@@ -60,30 +132,72 @@ def calculate_gpa(row, gpa_df) -> int:
         last_names_list = last_names_str.split(' ')
         first_names_list = first_names_str.split(' ')
 
-        first_name = first_names_list[:1]
-        last_name = last_names_list[-1:]
-    
-    
-    
-    return 0.0
+        first_name = first_names_list[0]
+        last_name = last_names_list[-1]
+
+    return first_name, last_name
+
+def calculate_average_gpa(df):
+    df = df.drop('CNT_W', axis=1)
+    grade_counts = df[grade_columns].sum().tolist()
+
+    if all(count == 0 for count in grade_counts):
+        # If all grade_counts are zero return None
+        return None
+    average_gpa = np.dot(gpa_values, grade_counts) / np.sum(grade_counts)
+
+    return average_gpa
+
+# This will pass in a row from the terms_df, and calculate gpa based on gpa_df
+def calculate_gpa(row, gpa_df):
+    subject = get_subject(row['subject'])
+    first_name, last_name = get_instructor(row['instructor'])
+    match_df = None
+
+    if not first_name:
+        # Find the average gpa of the course if it exists, otherwise None
+        match_df = gpa_df[(gpa_df['CODE'] == subject)]
+        if not match_df.empty:
+            return calculate_average_gpa(match_df)
+            # No data for the professor or subject was found
+        else:
+            return None
+
+    # The course has an instructor, look for the instructor
+    name_df = gpa_df[(gpa_df['INSTRUCTOR_FIRST'].str.contains(first_name)) & 
+                     (gpa_df['INSTRUCTOR_LAST'].str.contains(last_name))]
+    # If no instructor found, look for last name with same course taught
+    if name_df.empty:
+        name_df = gpa_df[(gpa_df['INSTRUCTOR_LAST'].str.contains(last_name)) &
+                         (gpa_df['CODE'].str.contains(subject))]
+    if not name_df.empty:
+        # We found the instructor first, last
+        match_df = name_df[(name_df['CODE'] == subject)]
+        if not match_df.empty:
+            return calculate_average_gpa(match_df)  # We found the best match
+        else:
+            return calculate_average_gpa(name_df)   # No course match, return instructor gpa
+    else:
+        return None
 
 def main():
     if not os.path.exists(gpa_pickle_file):
         excel_to_pickle()
     gpa_df = pd.read_pickle(gpa_pickle_file)
-    print(gpa_df)
-    terms, year = filter_terms(fetch_terms_list())
+    gpa_df.dropna(subset=['CNT_A'], inplace=True)
+    terms, _ = filter_terms(fetch_terms_list())
 
     # Iterate over every term folder in data dir
     for term in terms:
+        print(f"Calculating {term}, this may take a minute...")
         term_directory = os.path.join(data_directory, term)
         term_pkl = os.path.join(term_directory, term + '.pkl')
         term_df = pd.read_pickle(term_pkl)
 
         # Calculate the gpa for every row
         term_df['gpa'] = term_df.apply(calculate_gpa, axis=1, gpa_df=gpa_df)
-
-        print(term_df)
+        term_df.to_pickle(term_pkl)
+        print(f"Succesfully saved {term_pkl}!")
 
 if __name__ == "__main__":
     main()
