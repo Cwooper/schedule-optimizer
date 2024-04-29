f1a243f9c51eadb2d72eacf9285b77eb9a22389a
cwooper <cwooperm@gmail.com>
2024-04-29 13:51:12 -0700

    finished changing grades.pkl processing

--- backend/gpa_processor.py
+++ backend/gpa_processor.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+import numpy as np
 import pandas as pd
 from Course import Course
 from data_refresh import fetch_terms_list, filter_terms
@@ -19,8 +20,24 @@ def excel_to_pickle():
     if not os.path.exists(gpa_excel_file):
         print(f"Error: File not found: {gpa_excel_file}.")
         exit(1)
+    print("Loading Excel...")
     df = pd.read_excel(gpa_excel_file)
     df = df.drop(columns_to_drop, axis=1)
+
+    for index, row in df.iterrows():
+        professor_name = row['PROFESSOR']
+        if professor_name and isinstance(professor_name, str):
+            professor_name_split = professor_name.split(' ')
+            instructor_first = professor_name_split[:1]
+            instructor_last = professor_name_split[-1:]
+        else:
+            instructor_first = None
+            instructor_last = None
+        
+        df.at[index, 'INSTRUCTOR_FIRST'] = instructor_first[0] if instructor_first else None
+        df.at[index, 'INSTRUCTOR_LAST'] = instructor_last[0] if instructor_last else None
+
+    df = df.drop('PROFESSOR', axis=1)
     df.to_pickle(gpa_pickle_file)
     print(f"Created {gpa_pickle_file}.")
 
@@ -36,7 +53,7 @@ def calculate_gpa(row, gpa_df) -> int:
     last_name = None
 
     if ', ' in instructor:
-        names = instructor.split(', ')
+        names = instructor.split(',')
         last_names_str = names[0].strip()
         first_names_str = names[1].strip()
 
@@ -54,6 +71,7 @@ def main():
     if not os.path.exists(gpa_pickle_file):
         excel_to_pickle()
     gpa_df = pd.read_pickle(gpa_pickle_file)
+    print(gpa_df)
     terms, year = filter_terms(fetch_terms_list())
 
     # Iterate over every term folder in data dir
