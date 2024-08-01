a31646f460daefbbb3127c780d77444a0bb26884
cwooper <cwooperm@gmail.com>
2024-07-31 17:19:23 -0700

    Started scraper, updated protobufs

---
+++ go-backend/internal/scraper/scraper.go
@@ -0,0 +1,13 @@
+// Package scraper for scraping course data and saving it
+package scraper
+
+import (
+	"http"
+	"encoding/csv"
+	"encoding/json"
+	"os"
+
+	"schedule-optimizer/internal/models"
+)
+
+
