ba085fcd928b82cabfae0fb74559b0461fed5d5f
cwooper <cwooperm@gmail.com>
2024-07-31 18:30:59 -0700

    Added core webscraper functionality

--- go-backend/internal/utils/utils.go
+++ go-backend/internal/utils/utils.go
@@ -1,15 +1,43 @@
 // Package utils for "modular" hard-coding and re-used functions
 package utils
 
-import "math"
+import (
+	"math"
+	"os"
+	"path/filepath"
+)
 
 const (
 	ROUND       = 2    // Round to x decimal places
 	MINS_IN_DAY = 1440 // Number of minutes in a day (60 * 24)
 
 	MIN_COURSES = 1 // Minimum courses in a single schedule
-	MAX_COURSES = 7	// Maximum courses in a single schedule
+	MAX_COURSES = 7 // Maximum courses in a single schedule
+
+	MAX_SUBJECT_WAIT = 30 // Days
+	MAX_COURSE_WAIT  = 1  // Days
+	MAX_TERM_WAIT    = 10 // Days
+
+	TIME_FORMAT = "2000-01-01 00:00:00"
+
+	URL = "https://web4u.banner.wwu.edu/pls/wwis/wwskcfnd.TimeTable"
 )
+
+var DataDirectory string
+
+func init() {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	DataDirectory = filepath.Join(wd, "data")
+
+	// Ensure that the directory exists
+	if err := os.MkdirAll(DataDirectory, os.ModePerm); err != nil {
+		panic(err)
+	}
+}
 
 // Helper function to round float64 to n decimal places
 func Round(x float64) float64 {
