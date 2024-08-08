2c77d30ea66c5b4e566230f92c6d422f83c2e149
cwooper <cwooperm@gmail.com>
2024-08-08 15:42:53 -0700

    Refactored backend

---
+++ backend/internal/scraper/extract.go
@@ -0,0 +1,329 @@
+package scraper
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+
+	"github.com/cwooper/schedule-optimizer/internal/models"
+	"github.com/cwooper/schedule-optimizer/internal/utils"
+)
+
+var (
+	httpClient  *http.Client
+	timePattern = regexp.MustCompile(`(\d{1,2}):(\d{2})\s*(AM|PM)?`)
+)
+
+func init() {
+	httpClient = &http.Client{
+		Timeout: time.Second * 10,
+	}
+}
+
+// convertTimes converts times format from "09:00-01:50 pm" to 900, 1350
+func convertTimes(times string) (int, int, error) {
+	timeParts := strings.Split(times, " ")
+	if len(timeParts) != 2 {
+		return 0, 0, fmt.Errorf("invalid time format: %s", times)
+	}
+
+	amPm := timeParts[1]
+	timeStartEnd := strings.Split(timeParts[0], "-")
+	if len(timeStartEnd) != 2 {
+		return 0, 0, fmt.Errorf("invalid time range format: %s", timeParts[0])
+	}
+
+	startTime := strings.ReplaceAll(timeStartEnd[0], ":", "")
+	endTime := strings.ReplaceAll(timeStartEnd[1], ":", "")
+
+	startTimeInt, err := strconv.Atoi(startTime)
+	if err != nil {
+		return 0, 0, fmt.Errorf("invalid start time: %s", startTime)
+	}
+
+	endTimeInt, err := strconv.Atoi(endTime)
+	if err != nil {
+		return 0, 0, fmt.Errorf("invalid end time: %s", endTime)
+	}
+
+	// Convert times to military time
+	if strings.ToLower(amPm) == "pm" {
+		if endTimeInt < 1200 {
+			endTimeInt += 1200
+		}
+		// If a class starts before 8, it must be a pm, unless
+		// it's bigger than the end_time when making it a pm
+		if startTimeInt < 800 && startTimeInt+1200 < endTimeInt {
+			startTimeInt += 1200
+		}
+	}
+
+	return startTimeInt, endTimeInt, nil
+}
+
+// Extracts the header from the string parts and the given rows
+func extractHeader(parts []string, rows *goquery.Selection) (*models.Course, error) {
+	subject := strings.TrimSpace(parts[0])
+	title := strings.TrimSpace(parts[1])
+	credits := strings.TrimSpace(strings.ReplaceAll(parts[2], "cr", ""))
+
+	prerequisites := ""
+	for i := 1; i < rows.Length(); i++ {
+		prerequisites += strings.TrimSpace(rows.Eq(i).Text())
+		if i != rows.Length()-1 { // Add spaces between rows
+			prerequisites += " "
+		}
+	}
+
+	return &models.Course{
+		Subject:       subject,
+		Title:         title,
+		Credits:       credits,
+		Prerequisites: prerequisites,
+	}, nil
+}
+
+// Extracts a course from the cells and fills them into the provided course
+func extractCourse(cells *goquery.Selection, course *models.Course) error {
+	// quarter := strings.TrimSpace(cells.Eq(0).Text()) // Unused
+	crn := strings.TrimSpace(cells.Eq(1).Text())
+	days := strings.TrimSpace(cells.Eq(2).Text())
+	times := strings.TrimSpace(cells.Eq(3).Text())
+	instructor := strings.TrimSpace(cells.Eq(4).Text())
+	location := strings.TrimSpace(cells.Eq(5).Text())
+	additionalFees := strings.TrimSpace(cells.Eq(6).Text())
+	capacity := strings.TrimSpace(cells.Eq(7).Text())
+	enrolled := strings.TrimSpace(cells.Eq(8).Text())
+	available := strings.TrimSpace(cells.Eq(9).Text())
+	// waitlist := strings.TrimSpace(cells.Eq(10).Text()) // Unused
+	restrictions := strings.TrimSpace(cells.Eq(11).Text())
+	attributes := strings.TrimSpace(cells.Eq(12).Text())
+
+	var startTime, endTime int
+	isAsync := false
+	isTimeTBD := false
+	if times != "" && timePattern.MatchString(times) {
+		// manually declare error to not overwrite startTime and endTime
+		var err error
+		startTime, endTime, err = convertTimes(times)
+		if err != nil {
+			return fmt.Errorf("failed to parse %v: %w", times, err)
+		}
+	} else if times == "N/A" {
+		isAsync = true
+	} else if times == "TBD" {
+		isTimeTBD = true
+	}
+
+	// Fill the session block
+	session := models.Session{
+		Days:       days,
+		StartTime:  startTime,
+		EndTime:    endTime,
+		Instructor: instructor,
+		Location:   location,
+		IsAsync:    isAsync,
+		IsTimeTBD:  isTimeTBD,
+	}
+
+	// Convert all of the needed number strings to ints
+	nums, err := convertMultiple(crn, capacity, enrolled, available)
+	if err != nil {
+		return fmt.Errorf("error converting course numbers: %w", err)
+	}
+
+	course.CRN = nums[0]
+	course.Sessions = append(course.Sessions, session)
+	course.AdditionalFees = normalizeSpaces(additionalFees)
+	course.Capacity = nums[1]
+	course.Enrolled = nums[2]
+	course.AvailableSeats = nums[3]
+	course.Restrictions = restrictions
+	course.Attributes = attributes
+
+	return nil
+}
+
+// Extracts a session from the cells and fills them into the provided course
+func extractSession(cells *goquery.Selection, course *models.Course) error {
+	days := strings.TrimSpace(cells.Eq(2).Text())
+	times := strings.TrimSpace(cells.Eq(3).Text())
+	instructor := strings.TrimSpace(cells.Eq(4).Text())
+	location := strings.TrimSpace(cells.Eq(5).Text())
+	// All other data does not exist for labs
+
+	var startTime, endTime int
+	isAsync := false
+	isTimeTBD := false
+	if times != "" && timePattern.MatchString(times) {
+		// manually declare error to not overwrite startTime and endTime
+		var err error
+		startTime, endTime, err = convertTimes(times)
+		if err != nil {
+			return fmt.Errorf("failed to parse %v: %w", times, err)
+		}
+	} else if times == "N/A" {
+		isAsync = true
+	} else if times == "TBD" {
+		isTimeTBD = true
+	}
+
+	session := models.Session{
+		Days:       days,
+		StartTime:  startTime,
+		EndTime:    endTime,
+		Instructor: instructor,
+		Location:   location,
+		IsAsync:    isAsync,
+		IsTimeTBD:  isTimeTBD,
+	}
+
+	course.Sessions = append(course.Sessions, session)
+
+	return nil
+}
+
+// Gets the courses from the given table
+//
+//		Format of a course table:
+//		<table> Header Information </table> (if fieldformatboldtext in <td>)
+//		<div></div>
+//		<table> Course Information <table> (if 13 <td> elements in <tr>)
+//		<table> Session Information <table> (if 12 <td> elements in <tr>)
+//		The above two can repeat in any order
+//	 The above then repeats any number of times
+func getCoursesFromTables(doc *goquery.Document) ([]models.Course, error) {
+	lastCourse := models.NewCourse()
+	var courses []models.Course
+	var err error
+
+	// Iterate over every table
+	doc.Find("table").Each(func(i int, tableSelection *goquery.Selection) {
+		rows := tableSelection.Find("tr") // Used in the header only
+		if strings.Contains(rows.Find("td").First().Text(), "Term") {
+			return // This is an info row
+		}
+
+		boldTextCells := tableSelection.Find("td.fieldformatboldtext")
+		if boldTextCells.Length() > 0 {
+			headerText := strings.TrimSpace(boldTextCells.Text())
+
+			// Extract the parts of the header's td.fieldformatboldtext
+			title := boldTextCells.Find("a").Text()
+			if title != "" {
+				beforeAfter := strings.SplitN(headerText, title, 2)
+				// This is a new header, process the previous course if exists
+				if lastCourse.Sessions != nil {
+					courses = append(courses, *lastCourse)
+				}
+				// Extract new header
+
+				parts := []string{beforeAfter[0], title, beforeAfter[1]}
+				lastCourse, err = extractHeader(parts, rows)
+				if err != nil {
+					fmt.Printf("error extracting header: %v\n", err)
+					return
+				}
+			}
+		} else {
+			// This table might contain course or session information
+			tableSelection.Find("tr").Each(func(j int, row *goquery.Selection) {
+				cells := row.Find("td")
+				cellCount := cells.Length()
+
+				if cellCount == 13 {
+					// This is a course row
+					if lastCourse.Sessions != nil {
+						// Append the course if it's a main session
+						courses = append(courses, *lastCourse)
+					}
+
+					// Reset the last course for the next one
+					if lastCourse.Sessions != nil {
+						lastCourse = &models.Course{
+							Subject:       lastCourse.Subject,
+							Title:         lastCourse.Title,
+							Credits:       lastCourse.Credits,
+							Prerequisites: lastCourse.Prerequisites,
+						}
+					}
+
+					err = extractCourse(cells, lastCourse)
+					if err != nil {
+						fmt.Printf("error extracting course %s: %v\n", lastCourse.Subject, err)
+						return
+					}
+				} else if cellCount == 12 && lastCourse.Sessions != nil {
+					err = extractSession(cells, lastCourse)
+					if err != nil {
+						fmt.Printf("error extracting session: %v\n", err)
+						return
+					}
+				}
+			})
+		}
+	})
+
+	// Append the last course if it exists
+	if lastCourse.Sessions != nil {
+		courses = append(courses, *lastCourse)
+	}
+
+	return courses, nil
+}
+
+// Returns all of the courses found at the given url specified
+// by the subject, term and year
+func getSubjectFromURL(subject, term, year string) ([]models.Course, error) {
+	// Create url.Values to hold the payload
+	data := url.Values{}
+	data.Set("term", term)
+	data.Set("curr_yr", year)
+	data.Set("subj", subject)
+
+	// Create a new request with the encoded form data
+	encodedData := data.Encode()
+	req, err := http.NewRequest("POST", utils.URL, strings.NewReader(encodedData))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	// Set the correct Content-Type for form data
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error: status code %d accessing %s with payload %v",
+			resp.StatusCode, utils.URL, encodedData)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(body)))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse HTML: %w", err)
+	}
+
+	// Get the course tables from the response body
+	courses, err := getCoursesFromTables(doc)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get courses from tables: %w", err)
+	}
+
+	return courses, nil
+}
---
+++ backend/internal/scraper/scraper.go
@@ -0,0 +1,249 @@
+// Package scraper for scraping course data and saving it
+package scraper
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/cwooper/schedule-optimizer/internal/gpa"
+	"github.com/cwooper/schedule-optimizer/internal/models"
+	"github.com/cwooper/schedule-optimizer/internal/utils"
+	"github.com/cwooper/schedule-optimizer/pkg/protoutils"
+)
+
+// Enum for subjects and terms
+const (
+	Subject int = iota
+	Terms
+)
+
+// Gets the subjects/terms from url
+func getSelectListFromURL(option string) ([]string, error) {
+	resp, err := http.Get(utils.URL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get URL: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("error: status code %d accessing %s", resp.StatusCode, utils.URL)
+	}
+
+	// Parse the HTML
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse HTML: %w", err)
+	}
+
+	// Find the select subject element and store them into a subjects slice
+	selectOption := fmt.Sprintf("select#%s option", option)
+	var subjects []string
+	doc.Find(selectOption).Each(func(i int, s *goquery.Selection) {
+		if value, exists := s.Attr("value"); exists {
+			subjects = append(subjects, value)
+		}
+	})
+
+	return subjects, nil
+}
+
+// Gets the given list from file or url dpeending on time since file creation
+func getList(option int) ([]string, error) {
+	// Create variables based upon type of get request
+	var filename string
+	var htmlID string
+	var waitTime int
+	if option == Subject {
+		filename = "subjects.txt"
+		htmlID = utils.SUBJECT_ID
+		waitTime = utils.MAX_SUBJECT_WAIT
+	} else {
+		filename = "terms.txt"
+		htmlID = utils.TERM_ID
+		waitTime = utils.MAX_TERM_WAIT
+	}
+
+	var lines []string
+	file := filepath.Join(utils.DataDirectory, filename)
+	_, err := os.Stat(file)
+
+	if err == nil {
+		// Parse the file and get items from file
+		lines, err = fileToLines(file)
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert %v to lines: %w", file, err)
+		}
+
+		fileTime, err := time.Parse(utils.TIME_FORMAT, lines[0])
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse time in %v: %w", lines[0], err)
+		}
+
+		// If file creation to now is longer than MAX wait time defined in utils
+		if time.Since(fileTime) < time.Duration(waitTime)*24*time.Hour {
+			return lines[1:], nil
+		}
+	}
+
+	// This will only run if all of the above fails
+	// Get the items from url
+	lines, err = getSelectListFromURL(htmlID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get subjects from url: %w", err)
+	}
+
+	// Save the file for re-use
+	linesToFile(lines, file)
+	fmt.Printf("Longer than %d days, saved %v\n", waitTime, file)
+
+	return lines, nil
+}
+
+// Filters the list of turns and returns the current year code
+func filterTerms(terms []string) ([]string, string, error) {
+	currentTerm := fmt.Sprintf("%d00", time.Now().Year())
+
+	var filteredTerms []string
+	for _, term := range terms {
+		if term >= currentTerm && term != "All" {
+			filteredTerms = append(filteredTerms, term)
+		}
+	}
+
+	if len(filteredTerms) == 0 {
+		return nil, "", fmt.Errorf("no valid terms found")
+	}
+
+	largestTerm := filteredTerms[0]
+	for _, term := range filteredTerms {
+		if term > largestTerm {
+			largestTerm = term
+		}
+	}
+
+	yearHigh, err := strconv.Atoi(largestTerm[2:4])
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to parse year: %w", err)
+	}
+
+	yearLow := yearHigh - 1
+	year := fmt.Sprintf("%02d%02d", yearLow, yearHigh)
+	return filteredTerms, year, nil
+}
+
+// Returns a list of all courses found for this term
+func getCoursesFromURL(subjects []string, term, year string) ([]models.Course, error) {
+	// About 1500 courses per term, 2000 to be efficient
+	var courseList []models.Course
+
+	fmt.Printf("\nProcessing %s: ", term)
+	for _, subject := range subjects {
+		fmt.Printf("%v ", subject)
+		newCourses, err := getSubjectFromURL(subject, term, year)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get courses: %w", err)
+		}
+		if len(newCourses) > 0 {
+			courseList = append(courseList, newCourses...)
+		}
+	}
+	fmt.Printf("\n")
+
+	return courseList, nil
+}
+
+// Gets the courses for the given term, returns the total courses found
+func getCourses(subjects []string, term, year string) (int, error) {
+	termFile := filepath.Join(utils.DataDirectory, term+".pb")
+
+	// Check if term protobuf already exists
+	if _, err := os.Stat(termFile); err == nil {
+		// File exists, check its Pulltimestamp
+		existingProto, err := utils.LoadCoursesProtobuf(termFile)
+		if err != nil {
+			return 0, fmt.Errorf("failed to load existing protobuf: %w", err)
+		}
+
+		pullTime := existingProto.PullTimestamp.AsTime()
+		if time.Since(pullTime) < utils.MAX_COURSE_WAIT*24*time.Hour {
+			// Use existing data
+			courseList := protoutils.ProtoToCourses(existingProto)
+			// if term == "202520" { // Helper print function
+			// 	printCourseList(courseList)
+			// }
+			return len(courseList), nil
+		}
+	}
+
+	// Fetch new data, above didn't work out
+	courseList, err := getCoursesFromURL(subjects, term, year)
+	if err != nil {
+		return 0, fmt.Errorf("failed to get courses from url: %w", err)
+	}
+
+	start := time.Now()
+	err = gpa.GenerateGPA(&courseList)
+	if err != nil {
+		return 0, fmt.Errorf("failed to generate gpa for course list: %w", err)
+	}
+	duration := time.Since(start)
+	fmt.Printf("GenerateGPA took %v to execute\n", duration)
+
+	protobuf := protoutils.CoursesToProto(courseList)
+	protobuf.PullTimestamp = timestamppb.Now()
+
+	// Save the protobuf
+	if err := utils.SaveCoursesProtobuf(protobuf, termFile); err != nil {
+		return 0, fmt.Errorf("failed to save protobuf for term %s: %w", term, err)
+	}
+
+	fmt.Printf("%v: Found and saved %d courses to %v\n", term, len(courseList), termFile)
+
+	return len(courseList), nil
+}
+
+// Updates all courses in Term protobufs if deemed necessary
+// It is up to the user to process the gpa of each course for each term
+func UpdateCourses() error {
+	// Create data directory if it doesn't exist
+	if err := os.MkdirAll(utils.DataDirectory, os.ModePerm); err != nil {
+		return fmt.Errorf("failed to create data directory: %w", err)
+	}
+
+	subjects, err := getList(Subject)
+	if err != nil {
+		return fmt.Errorf("failed to get subjects list: %w", err)
+	}
+
+	terms, err := getList(Terms)
+	if err != nil {
+		return fmt.Errorf("failed to get terms list: %w", err)
+	}
+
+	terms, year, err := filterTerms(terms)
+	if err != nil {
+		return fmt.Errorf("failed to filter terms list: %w", err)
+	}
+
+	fmt.Printf("Found %d subjects\n", len(subjects))
+	fmt.Printf("Current terms: %v\n", terms)
+
+	count := 0
+	for _, term := range terms {
+		found, err := getCourses(subjects, term, year)
+		if err != nil {
+			return fmt.Errorf("failed to get courses: %w", err)
+		}
+		count += found
+	}
+
+	fmt.Printf("Found %d total courses\n", count)
+	return nil
+}
