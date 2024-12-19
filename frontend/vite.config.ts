1fc63ff817400acd45273ee192908ae7fe9e2440
cwooper <cwooperm@gmail.com>
2024-12-19 01:14:45 -0800

    Remove recharts from package lock and vite rollup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/schedule-optimizer/",
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          recharts: ["recharts"],
-        },
-      },
-    },
-  },
 });
